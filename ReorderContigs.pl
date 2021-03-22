5bf2f86b20051db585a76a37ae3aa53353a4fe9c
Alexander Julian <ajulian@hawk.iit.edu>
2021-03-21 21:18:31 -0500

    Comment and code cleanup

--- ReorderContigs.pl
+++ ReorderContigs.pl
@@ -3,21 +3,22 @@
 use strict; use warnings; use Getopt::Long qw(GetOptions); use File::Basename;
 
 my $name = 'ReorderContigs.pl';
-my $version = '0.1';
-my $updated = '2/15/2021';
+my $version = '0.1a';
+my $updated = '03/21/2021';
 my $usage = <<"EXIT";
 
-NAME        ${name}
-VERSION     ${version}
-UPDATED     ${updated}
-SYNOPSIS    This script sorts contigs by sequence length (longest to shortest) 
-            and renames them accordingly.
+NAME		${name}
+VERSION		${version}
+UPDATED		${updated}
+SYNOPSIS	This script sorts contigs by sequence length (longest to shortest) 
+			and renames them accordingly.
 
-COMMAND     ${name} -i E_hel.spades_assembly.fasta
+COMMAND		${name} \\
+			-i E_hel.spades_assembly.fasta
 
 USAGE
 
--i | --in       Fasta assembly file
+-i | --in	Fasta assembly file
 
 EXIT
 
@@ -38,12 +39,12 @@ open OUT,">","reordered.fasta";
 my $contig;
 my %sequences;
 while(my $line = <IN>){
-    chomp($line);
-    if($line =~ /^>(\S+?_\d+)/){
-        $contig = $1;
-        next;
-    }
-    $sequences{$contig} .= $line;
+	chomp($line);
+	if($line =~ /^>(\S+?_\d+)/){
+		$contig = $1;
+		next;
+	}
+	$sequences{$contig} .= $line;
 }
 
 ## Calculating number of padding zeros
@@ -53,18 +54,18 @@ my $width = length($nos);
 ## Get the length of the contig sequences
 my %order;
 foreach my $key (keys %sequences){
-    my $len = length($sequences{$key});
-    $order{$key} = $len;
+	my $len = length($sequences{$key});
+	$order{$key} = $len;
 }
 
 ## Print contig sequences from largest to smallest
 my $contig_num = sprintf("%0${width}d", 1);
 foreach my $key (sort { $order{$b} <=> $order{$a}} keys %order){
-    my $len = length($sequences{$key});
-    print OUT ">contig_$contig_num [length = $len bp]\n";
-    my @seq = unpack("(A60)*",$sequences{$key});
-    while (my $val = shift(@seq)){
-        print OUT "$val\n";
-    }
-    $contig_num++;
+	my $len = length($sequences{$key});
+	print OUT ">contig_$contig_num [length = $len bp]\n";
+	my @seq = unpack("(A60)*",$sequences{$key});
+	while (my $val = shift(@seq)){
+		print OUT "$val\n";
+	}
+	$contig_num++;
 }
